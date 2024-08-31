6dd92308af12c20d53193814441eaf9c8e30f6b7
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-08-31 09:25:27 -0300

    Add files via upload

---
+++ exemplos/MANIPULACAODESTRINGS.sql
@@ -0,0 +1,18 @@
+/*
+
+SELECT CONCAT(FirstName,' ', LastName) AS "Nome completo"
+FROM Person.Person
+
+SELECT UPPER(FirstName), LOWER (LastName)
+FROM Person.Person
+
+SELECT LEN(FirstName)
+FROM Person.Person
+
+SELECT FirstName, SUBSTRING(FirstName, 1,3)
+FROM Person.Person
+
+SELECT REPLACE(ProductNumber, '-', '#')
+FROM Production.Product
+
+*/
