eea736cad122fd068181e2774574c7681edb1472
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-09-01 13:19:32 -0300

    Add files via upload

---
+++ exemplos/OPERACOESMATEMATICAS.sql
@@ -0,0 +1,12 @@
+/*
+
+SELECT UnitPrice + LineTotal
+FROM Sales.SalesOrderDetail
+
+SELECT SQRT(LineTotal) AS "Raiz quadrada", LineTotal
+FROM Sales.SalesOrderDetail
+
+SELECT ROUND(ListPrice, 1), ListPrice
+FROM Production.Product
+
+*/
