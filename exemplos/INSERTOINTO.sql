026e32d7ac88836d0e921dd8d6ef1bbf31359ae4
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-09-08 08:37:22 -0300

    Add files via upload

---
+++ exemplos/INSERTOINTO.sql
@@ -0,0 +1,22 @@
+/*
+
+INSERT INTO nomeTabela(coluna1, coluna2,...)
+VALUES(valor1, valor2)
+
+INSERT INTO TabelaA (coluna1)
+SELECT coluna2
+FROM TabelaB
+
+INSERT INTO aula(id, nome)
+VALUES(1,'aula 1')
+(2, 'Aula 2'),
+(3, 'Aula 3'),
+(4, 'Aula 4');
+
+SELECT * INTO tabelaInstagram
+FROM videoInstagram
+
+SELECT *
+FROM tabelaInstagram
+
+*/
