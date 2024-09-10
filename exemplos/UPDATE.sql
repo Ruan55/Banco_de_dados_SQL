2e84f747bce0517e2bf085c587266bbd94564969
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-09-10 14:46:43 -0300

    Add files via upload

---
+++ exemplos/UPDATE.sql
@@ -0,0 +1,16 @@
+/*
+
+UPDATE nomeTabela
+SET coluna1 = valor1
+    coluna2 = valor2
+WHERE condicao <- OBRIGATÓRIO
+
+UPDATE aula
+SET nome = 'mudei'
+WHERE id = 3
+
+UPDATE aula
+SET nome = 'mudado'
+WHERE id = 4
+
+*/
