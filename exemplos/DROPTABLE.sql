3398aa712af16a4926851c0ca65394b31befd52e
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-09-14 09:16:58 -0300

    Add files via upload

---
+++ exemplos/DROPTABLE.sql
@@ -0,0 +1,10 @@
+/*
+
+DROP TABLE nomeTabela
+
+DROP TABLE ErrorLog
+
+// uma operação Data Manipulation Language (DML) que exclui todas as linhas de uma tabela sem causar uma ação acionada.
+TRUNCATE TABLE Person.Password
+
+*/
