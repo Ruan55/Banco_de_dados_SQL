5de4b97c98c193fea0917830a6b19cd7bd7d6765
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-09-15 14:31:43 -0300

    Add files via upload

---
+++ exemplos/CHECKCONSTRAINT.sql
@@ -0,0 +1,17 @@
+/*
+
+CREATE TABLE nomeMotorista(
+Id INT NOT NULL,
+Nome VARCHAR(255) NOT NULL,
+Idade INT CHECK (Idade >= 18)
+);
+
+CREATE TABLE alistamentoMilitar(
+Id INT NOT NULL,
+Nome VARCHAR(200) NOT NULL,
+Idade INT CHECK (Idade >= 18)
+);
+
+
+
+*/
