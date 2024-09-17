df20baa64183de2205566c1c0b9bd75dda73b023
Ruan55 <114105714+Ruan55@users.noreply.github.com>
2024-09-17 15:01:12 -0300

    Add files via upload

---
+++ SQLQuery45.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Aviao(
+Id INT NOT NULL,
+Nome VARCHAR(255) NOT NULL,
+Idade INT CHECK (Idade >= 18),
+NumeroModelo INT NOT NULL UNIQUE
+);
+
+INSERT INTO Aviao(Id, Nome, Idade, NumeroModelo) VALUES (5, 'Ruan', 22, 18323)
+
+SELECT * 
+FROM Aviao
